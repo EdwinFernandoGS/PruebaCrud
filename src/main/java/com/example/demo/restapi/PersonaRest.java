0d1de782abad60ab574754715771461a8c88b526
EdwinFernandoGS <edwinfer09@hotmail.com>
2022-05-09 14:54:26 -0500

    Se borran librerias innecesarias

--- src/main/java/com/example/demo/restapi/PersonaRest.java
+++ src/main/java/com/example/demo/restapi/PersonaRest.java
@@ -1,6 +1,5 @@
 package com.example.demo.restapi;
 
-import java.io.Console;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -8,15 +7,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Persona;
